a18204073c94efe0a3005d2bedd425a8b53a4ecd
かわゆた <maqueen@unknown-2.local>
2017-03-07 23:37:51 +0900

    add Responsive

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,9 +2,9 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!
   
   def show 
-    @messages = Message.all
-@my_account_name = current_user.username
-@my_account_id = current_user.id
+    	@messages = Message.all
+	@my_account_name = current_user.username
+	@my_account_id = current_user.id
 	@userlist = User.all
 	@userallid = @userlist.map(&:id)
 	@userallid.delete(@my_account_id)
