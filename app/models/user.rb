970bbc009a3740b8b2bbcf18dee2acee355e397a
mnshey <k216q013@stu.kyoto-seika.ac.jp>
2017-03-30 23:40:21 +0900

    bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
     presence: true
     
     validates :username,
-    length: { in: 5..30 } ,
+    length: { in: 5..30 },
     uniqueness: true,
     presence: true
     
